87745a5df913f72bd2faa21acfc55e483df5e79a
Sivaramvt <Sivaramvt@gmail.com>
2015-03-14 08:49:22 +0530

    Adding the project template.
    Project created using Maven archetype
    https://repo.adobe.com/nexus/content/groups/public/com/day/jcr/vault/mul
    timodule-content-package-archetype/

---
+++ cp-app-patterns/bundle/src/main/java/com/computepatterns/apppatterns/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.computepatterns.apppatterns;
+
+import aQute.bnd.annotation.Version;
---
+++ cp-app-patterns/bundle/src/test/java/com/computepatterns/apppatterns/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.computepatterns.apppatterns;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
