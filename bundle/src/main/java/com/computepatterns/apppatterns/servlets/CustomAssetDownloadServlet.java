b40112bb8f4cd2d7e94274f2942f5002d7f90214
Sivaramvt <Sivaramvt@gmail.com>
2015-06-19 19:47:45 +0530

    Custom Asset download servlet - Implementing using OptingServlet mechanism.

--- bundle/src/main/java/com/computepatterns/apppatterns/servlets/CustomAssetDownloadServlet.java
+++ bundle/src/main/java/com/computepatterns/apppatterns/servlets/CustomAssetDownloadServlet.java
@@ -7,39 +7,53 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.OptingServlet;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * Custom asset-download servlet. Overrides the existing asset download servlet of AEM {@link com.day.cq.dam.core.impl.servlet.AssetDownloadServlet}.
  * This is for demonstrating how OOTB servlet in AEM can be overriden and hence asset download specific implementation not supplied.
  * OOTB {@link com.day.cq.dam.core.impl.servlet.AssetDownloadServlet}'s OSGi properties have been copied down to this custom servlet with the
- * resource type pointed to dam:Asset to get preference over the OOTB servlet in the servlet resolution process implemented in {@link org.apache.sling.servlets.resolver.internal.SlingServletResolver}
+ * resource type pointed to dam:Asset to get preference over the OOTB servlet in the servlet resolution process implemented in {@link org.apache.sling.servlets.resolver.internal.SlingServletResolver}.
+ * This servlet accepts download requests for single asset. In case of multiple asset download, it doesn't accept the request.
  */
 @Component(metatype = false)
-@Service()
+@Service
 @Properties(value = {
         @Property(name = "sling.servlet.resourceTypes", value = DamConstants.NT_DAM_ASSET),
         @Property(name = "sling.servlet.methods", value = {"GET", "POST"}),
         @Property(name = "sling.servlet.selectors", value = "assetdownload")
 })
-public class CustomAssetDownloadServlet extends SlingAllMethodsServlet {
+public class CustomAssetDownloadServlet extends SlingAllMethodsServlet implements OptingServlet {
     private static final Logger log = LoggerFactory.getLogger(CustomAssetDownloadServlet.class);
 
     protected void doGet(SlingHttpServletRequest request,
                          SlingHttpServletResponse response)
             throws ServletException, IOException {
-        // Asset download specific implementation not provided as the purpose is to show how OOTB servlet in AEM can be overidden.
-        if (null != request.getRequestParameter("path"))
-            log.info("Custom download servlet being invoked. Path(s) requested for asset download - {}", Arrays.asList(request.getRequestParameters("path")));
-        else
-            log.info("Custom download servlet being invoked. Path(s) requested for asset download - {}", request.getResource().getPath());
+        log.info("Custom download servlet being invoked. Path(s) requested for asset download - {}", request.getResource().getPath());
+    }
 
-        return;
+    /**
+     * Examines the request, and return <code>true</code> if this servlet is
+     * willing to handle the request. If <code>false</code> is returned, the
+     * request will be ignored by this servlet, and may be handled by other
+     * servlets.
+     *
+     * @param request The request to examine
+     * @return <code>true</code> if this servlet will handle the request,
+     * <code>false</code> otherwise
+     */
+    @Override
+    public boolean accepts(SlingHttpServletRequest request) {
+        if (null != request.getRequestParameter("path")) { //multiple asset download case
+            log.info("Custom download servlet being invoked. Multiple assets chosen and hence not accepting.");
+            return false;
+        }
+        return true;
     }
 }
